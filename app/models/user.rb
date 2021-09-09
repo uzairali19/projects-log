b028ffa9b19dca6ba20f15d44944b3ca0470e770
Uzair <uzairali19b@gmail.com>
2021-09-09 17:24:15 +0500

    fix linter error and update routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :name, presence: true, length: { maximum: 50 }, unique: :true
+  validates :name, presence: true, length: { maximum: 50 }, uniqueness: true
 
   has_many :projects
   has_many :courses
